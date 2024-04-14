4bfa6bef258fb9e49dd79a191324593be07e1e8f
micheleAndreata <michele.andreata@studenti.unimi.it>
2024-04-14 11:13:52 +0200

    Added other impls

--- src/bin/evaluate.rs
+++ src/bin/evaluate.rs
@@ -1,24 +1,29 @@
-use bitm::RankSelect101111;
 use rand::{rngs::SmallRng, SeedableRng};
-use sux::rank_sel::{Rank9Sel, SimpleSelect};
 use valcurt::evaluator::Evaluator;
 
 fn main() {
     let rng = SmallRng::seed_from_u64(0);
     let repetitions = 10;
-    let lens = vec![1_000_000];
+    let lens = vec![1_000_000_000];
     let densities = vec![0.5];
     let uniform = false;
     let iterations = 80_000_000;
 
-    let mut simple_evaluator: Evaluator<SimpleSelect> = Evaluator::new(rng.clone());
+    let mut simple_eval: Evaluator<sux::rank_sel::SimpleSelect> = Evaluator::new(rng.clone());
 
-    let mut rank9sel_evaluator: Evaluator<Rank9Sel> = Evaluator::new(rng.clone());
+    let mut rank9sel_eval: Evaluator<sux::rank_sel::Rank9Sel> = Evaluator::new(rng.clone());
 
-    let mut rankselect101111_evaluator: Evaluator<RankSelect101111> = Evaluator::new(rng.clone());
+    let mut rankselect101111_eval: Evaluator<bitm::RankSelect101111> = Evaluator::new(rng.clone());
+
+    let mut sucds_rank9sel_eval: Evaluator<sucds::bit_vectors::rank9sel::Rank9Sel> =
+        Evaluator::new(rng.clone());
+
+    let mut succinct_sel_eval: Evaluator<
+        succinct::BinSearchSelect<succinct::rank::Rank9<succinct::BitVector<u64>>>,
+    > = Evaluator::new(rng.clone());
 
     println!("SimpleSelect");
-    simple_evaluator.bench(
+    simple_eval.bench(
         "SimpleSelect",
         &lens,
         &densities,
@@ -27,7 +32,7 @@ fn main() {
         iterations,
     );
     println!("Rank9Sel");
-    rank9sel_evaluator.bench(
+    rank9sel_eval.bench(
         "Rank9Sel",
         &lens,
         &densities,
@@ -36,8 +41,28 @@ fn main() {
         iterations,
     );
     println!("RankSelect101111");
-    rankselect101111_evaluator.bench(
+    rankselect101111_eval.bench(
         "RankSelect101111",
+        &lens,
+        &densities,
+        uniform,
+        repetitions,
+        iterations,
+    );
+
+    println!("Sucds Rank9Sel");
+    sucds_rank9sel_eval.bench(
+        "SucdsRank9Sel",
+        &lens,
+        &densities,
+        uniform,
+        repetitions,
+        iterations,
+    );
+
+    println!("Succinct BinSearchSelect Rank9");
+    succinct_sel_eval.bench(
+        "SuccinctBinSearchSelectRank9",
         &lens,
         &densities,
         uniform,
